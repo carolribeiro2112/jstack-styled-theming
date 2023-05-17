1199e1a02b0c534bd7ad38ab14a1b5903f8121dc
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2023-05-17 09:53:42 -0300

    add style themes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './styles/global';
 import Layout from './components/Layout';
 
+import dark from './styles/themes/dark';
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyle />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 };
 
